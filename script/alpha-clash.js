36a6274cbaf176b0ab6a464f47ce23e043c3ddfa
Jafar Iqbal <Iqbal.jafer@ARTIC2.LOCAL>
2024-02-15 16:28:02 +0300

    next commit

---
+++ script/alpha-clash.js
@@ -0,0 +1,32 @@
+// fist way
+// function play(){
+//     // step-1: hide the home screen. to hide the screen add the class hidden to the home section
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // console.log(homeSection.classList);
+//     //show the playground
+
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden');
+// }
+
+// second way
+
+function continueGame() {
+  // step-1: generate a random alphabet
+  const alphabet = getARandomAlphabet();
+  console.log("Your random alphabet", alphabet);
+
+  // set randomly generated alphabet to the screen (show it)
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  currentAlphabetElement.innerText = alphabet;
+
+  // set background color
+  setBackgroundColorById(alphabet);
+}
+
+function play() {
+  hideElementById("home-screen");
+  showElementById("play-ground");
+  continueGame();
+}
